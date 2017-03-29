dcb44b048ebeaff5b7a618b16b90d174dfad4ec8
lboza91 <lboza91@iesnumancia.cat>
2017-03-29 12:06:58 +0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -22,8 +22,8 @@ $(document).on('deviceready', function() {
 	var mida_y_bola = mida_x_bola ;  // 36 ;
 	var posicio_x_bola = centre_x - ( mida_x_bola / 2 ) ;
 	var posicio_y_bola = centre_y - ( mida_y_bola / 2 ) ;
- 	
- 	
+		  
+				  
 	document.addEventListener("offline", function() { 
 		// alert("ara NO HI HA internet");
 	}, false);
@@ -33,31 +33,45 @@ $(document).on('deviceready', function() {
 	}, false); 
 	
 	document.addEventListener('touchstart', function(e) {
-	     alert("Clicat") ;
+	     	
+		var touchobj = e.changedTouches[0] ; // referència al primer punt tocat (pex: el primner dit)
+		startx = parseInt(touchobj.clientX) ; // quina és la posició x en referència al costat esquerra de la pantalla
+		starty = parseInt(touchobj.clientY) ;
+		
+		e.preventDefault() ;
+		//alert("Touch_x : " + startx + " --- Touch_y : " + starty);
+		
+		draw(startx,starty)	;
+		
 	});	
 	
 	document.addEventListener('touchmove', function(e) {
-	      alert("has arrastrat el dit");
+	      	
+		var touchobj = e.changedTouches[0] ; // referència al primer punt tocat (pex: el primner dit)
+		startx = parseInt(touchobj.clientX) ; // quina és la posició x en referència al costat esquerra de la pantalla
+		starty = parseInt(touchobj.clientY) ;
+		
+		e.preventDefault() ;
+		//alert("Touch_x : " + startx + " --- Touch_y : " + starty);
+		
 	 });
 	
-	var nivell = 1 ;
-	draw(amplada_pantalla_CSS,alcada_pantalla_CSS,posicio_x_bola,posicio_y_bola,mida_x_bola,mida_y_bola,nivell)	 ;
+	
 	
 });	
 	
 
 
-function draw(amplada_pantalla_CSS,alcada_pantalla_CSS,posicio_x_bola,posicio_y_bola,mida_x_bola,mida_y_bola,nivell) {
+function draw(startx,starty) {
 	
-		alert("cridada la funció DRAW");
+		// alert("cridada la funció DRAW");
 	
 		var canvas = document.getElementById('canvas');
 		var ctx = canvas.getContext('2d');
 		
 		 // Formas rectangulares
-          	ctx.fillRect(25,25,100,100);
-          	ctx.clearRect(45,45,60,60);
-	        ctx.strokeRect(50,50,50,50);
+          	ctx.fillRect(startx,starty,100,100);
+          
 		
 		
 		
